cd2b9ac2fcd5b44271796c4773cade3bf8688c2c
René <mail@renehahn.de>
2017-09-15 09:19:29 +0200

    adds drafts of new modelsmells based on java

---
+++ thesis.detection.smells/src/org/eclipse/emf/refactor/modelsmell/RedundantContainerRelation.java
@@ -0,0 +1,17 @@
+package org.eclipse.emf.refactor.modelsmell;
+
+import java.util.LinkedList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
+
+
+public final class RedundantContainerRelation implements IModelSmellFinder {
+
+	@Override
+	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
+		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
+		// TODO Fill results
+		return results;
+	}
+	
+}
