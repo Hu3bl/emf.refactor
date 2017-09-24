dbfe10d45654168f087aca69ddb7928afde45d26
René <mail@renehahn.de>
2017-09-24 14:11:13 +0200

    updates configuration

--- thesis.detection.smells/src/org/eclipse/emf/refactor/modelsmell/MultipathHierarchy.java
+++ thesis.detection.smells/src/org/eclipse/emf/refactor/modelsmell/MultipathHierarchy.java
@@ -9,9 +9,8 @@ import org.eclipse.emf.refactor.smells.interfaces.IModelSmellFinder;
 
 import thesis.detection.smells.util.DetectionHelper;
 
-
 public final class MultipathHierarchy implements IModelSmellFinder {
-		
+
 	@Override
 	public LinkedList<LinkedList<EObject>> findSmell(EObject root) {
 		LinkedList<LinkedList<EObject>> results = new LinkedList<LinkedList<EObject>>();
@@ -20,24 +19,24 @@ public final class MultipathHierarchy implements IModelSmellFinder {
 			if (cl.getESuperTypes().size() > 1) {
 				for (int i = 0; i < cl.getESuperTypes().size(); i++) {
 					EClass cl1 = cl.getESuperTypes().get(i);
-					for (int j = (i+1); j < cl.getESuperTypes().size(); j++) {
+					for (int j = (i + 1); j < cl.getESuperTypes().size(); j++) {
 						EClass cl2 = cl.getESuperTypes().get(j);
-							List<EClass> superclasses1 = DetectionHelper.getAllESuperclasses(cl1);
-							List<EClass> superclasses2 = DetectionHelper.getAllESuperclasses(cl2);
-							for (EClass superclass : superclasses1) {
-								if (superclasses2.contains(superclass)) {
-									LinkedList<EObject> result = new LinkedList<EObject>();
-									result.add(cl);
-									result.add(cl1);
-									result.add(cl2);
-									result.add(superclass);
-									results.add(result);
-								}
+						List<EClass> superclasses1 = DetectionHelper.getAllESuperclasses(cl1);
+						List<EClass> superclasses2 = DetectionHelper.getAllESuperclasses(cl2);
+						for (EClass superclass : superclasses1) {
+							if (superclasses2.contains(superclass)) {
+								LinkedList<EObject> result = new LinkedList<EObject>();
+								result.add(cl);
+								result.add(cl1);
+								result.add(cl2);
+								result.add(superclass);
+								results.add(result);
 							}
+						}
 					}
 				}
 			}
 		}
 		return results;
-	}	
+	}
 }
